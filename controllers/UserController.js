f090016c7a86cff7272f610b1d5fbbc59e34364a
dravenxiaokai <jspankai@vip.qq.com>
2017-09-27 21:38:42 +0800

    身份验证与签发 token

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,6 @@
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const store = (req, res) => {
     const userName = req.body.userName
@@ -19,6 +20,33 @@ const store = (req, res) => {
 
 }
 
+const auth = (req, res) => {
+    User.findOne({ userName: req.body.userName })
+        .then(user => {
+            if (!user) {
+                return Promise.reject({ message: '没找到用户' })
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(result => {
+                    if (result) {
+                        const payload = {
+                            userName: user.userName
+                        }
+                        const secret = 'I_LOVE_DRAVEN'
+                        const token = jwt.sign(payload, secret)
+
+                        res.send({ token })
+                    } else {
+                        res.status(401).send({ message: '未通过身份验证' })
+                    }
+                })
+        })
+        .catch(err => {
+            res.status(400).send(err)
+        })
+}
+
 module.exports = {
-    store
+    store,
+    auth
 }
