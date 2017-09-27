9e77bfb47599655839dc424398ecf64c2ec9e52b
dravenxiaokai <jspankai@vip.qq.com>
2017-09-28 00:35:23 +0800

    Promise,async,await,loki数据库

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 const multer = require('multer')
+const loki = require('lokijs')
+
+const db = new loki('uploads/uploads.json', { persistenceMethod: 'fs' })
+
+const loadCollection = (collectionName, db) => {
+    return new Promise(resolve => {
+        db.loadDatabase({}, () => {
+            const collection = db.getCollection(collectionName) || db.addCollection(collectionName)
+            resolve(collection)
+        })
+    })
+}
 
 const fileFilter = (req, file, callback) => {
     if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
@@ -11,8 +23,13 @@ const fileFilter = (req, file, callback) => {
 
 const upload = multer({ dest: 'uploads/', fileFilter })
 
-app.post('/profile', upload.single('avatar'), (req, res, next) => {
-    res.send(req.file)
+// nodejs 7 以后才可以使用 async
+app.post('/profile', upload.single('avatar'), async (req, res, next) => {
+    // res.send(req.file)
+    const collection = await loadCollection('uploads', db)
+    const result = collection.insert(req.file)
+    db.saveDatabase()
+    res.send(result)
 })
 
 // app.post('/photo/upload', upload.fields([{ name: 'photos', maxCount: 3 }])
