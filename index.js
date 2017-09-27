66ebb9fa742f1b8934564dcabbc804ee5334a94d
dravenxiaokai <jspankai@vip.qq.com>
2017-09-27 23:29:49 +0800

    文件上传过滤器

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+
+const fileFilter = (req, file, callback) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return callback(new Error('images only :)'), false)
+    }
+    callback(null, true)
+}
+
+const upload = multer({ dest: 'uploads/', fileFilter })
 
 app.post('/profile', upload.single('avatar'), (req, res, next) => {
     res.send(req.file)
@@ -10,6 +18,12 @@ app.post('/profile', upload.single('avatar'), (req, res, next) => {
 // app.post('/photo/upload', upload.fields([{ name: 'photos', maxCount: 3 }])
 app.post('/photos/upload', upload.array('photos', 3), (req, res, next) => {
     res.send(req.files)
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).send({
+        message: err.message
+    })
 })
 
 app.listen(8080, () => {
