e394f8360fb1fc10d25aee39192769aa8cb66f7e
dravenxiaokai <jspankai@vip.qq.com>
2017-09-27 21:21:21 +0800

    存储 hash 之后的密码

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,17 +1,22 @@
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 const store = (req, res) => {
     const userName = req.body.userName
-    const password = req.body.password
 
-    const user = new User({
-        userName,
-        password
-    })
+    bcrypt.hash(req.body.password, 10)
+        .then(password => {
 
-    user.save()
-        .then(() => res.send({ message: '注册成功' }))
-        .catch(err => console.log(err))
+            const user = new User({
+                userName,
+                password
+            })
+
+            user.save()
+                .then(() => res.send({ message: '注册成功' }))
+                .catch(err => console.log(err))
+        })
+
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const crypto = require('crypto')
-const hash = crypto.createHash('sha256')
+// const crypto = require('crypto')
+// const hash = crypto.createHash('sha256')
 
-hash.update('password')
-console.log(hash.digest('base64'))
+// hash.update('password')
+// console.log(hash.digest('base64'))
+
+const bcrypt = require('bcryptjs')
+
+const password = 'password'
+
+bcrypt.genSalt(10, (err, salt) => {
+    console.log('salt: ', salt)
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log('hash: ', hash)
+    })
+})
+
+const hashPassword = '$2a$10$D5HtOYGHAUuGjLzXiRFZJeo8IL1isvB4HwxWK4H6BYo9RqpT.agwK'
+const userInputPassword = 'password'
+ 
+bcrypt.compare(userInputPassword, hashPassword)
+    .then(result => console.log(result))
